69433af962a900537431e95d84b72517ebf84007
Jakob Asslaender <jakob.asslaender@nyumc.org>
2026-03-11 14:15:12 -0400

    remove init function and leave FFTW and BLAS number of threads untouched.

--- src/MRISubspaceRecon.jl
+++ src/MRISubspaceRecon.jl
@@ -12,13 +12,6 @@ using IterativeSolvers
 export NFFTNormalOp, calculate_coil_maps, calculate_backprojection, traj_kooshball, traj_kooshball_goldenratio, traj_2d_radial_goldenratio, traj_cartesian
 export FFTNormalOp, radial_grog!
 
-function __init__()
-    if Threads.nthreads() > 1
-      BLAS.set_num_threads(1)
-    end
-    FFTW.set_num_threads(Threads.nthreads())
-end
-
 include("GROG.jl")
 include("FFTNormalOp.jl")
 include("NFFTNormalOp.jl")
